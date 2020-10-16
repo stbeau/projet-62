a37205fbacc4610c46be127cb3e93bbc00c1107a
salim <1595651@bdeb.qc.ca>
2020-10-16 19:22:03 -0400

    Added config.py

---
+++ src/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+class Config:
+    
+    SRC_PATH = "../"
+    
+    DOWNLOAD_PATH = "https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/2019/"
+    INPUT_FILENAME = "full.csv.gz"
+    OUTPUT_PATH = "../data/raw"
+    OUTPUT_FILENAME = "dvf_2019.gz"
+
+    SOURCE_URL = os.path.join(DOWNLOAD_PATH,INPUT_FILENAME)
+    OUTPUT_FULLNAME = os.path.join(OUTPUT_PATH, OUTPUT_FILENAME)
+
+
+    METRIC_FILE_PATH = SRC_PATH / "metrics.json"
+
+
+
